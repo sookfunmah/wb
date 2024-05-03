b94040f9f837e12d1eca5d887ccd0da109b9e84d
sookfun <sookfun.mah@gmail.com>
2024-05-03 23:23:20 +0800

    added CORS configuration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 const cors = require ('cors')
 const {connect} = require ('mongoose')
-//require('dotenv').config()
+require('dotenv').config()
 
 const upload = require ('express-fileupload')
 const {notFound, errorHandler} = require ('./middleware/errorMiddleware')
@@ -10,15 +10,31 @@ const userRoutes = require('./routes/userRoutes')
 const postRoutes = require('./routes/postRoutes')
 
 const app = express();
+
+// CORS configuration
+const allowedOrigins = [
+    'https://yourfrontenddomain.com',
+    'http://localhost:3000' // Allow requests from your local development environment
+  ];
+  
+  app.use(cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }));
+
+
+
 app.use(express.json({extended:true}))
 app.use(express.urlencoded({extended:true}))
-//app.use(cors({credentials:true, origin: "http://localhost:3000"}))
+// app.use(cors({credentials:true, origin: "http://localhost:3000"}))
 
-const corsOptions = {
-    origin: 'https://wbfront-qfxzbl7l8-sook-funs-projects.vercel.app',
-    credentials: true // Enable credentials (e.g., cookies) in cross-origin requests
-};
-app.use(cors(corsOptions));
+
 
 
 app.use(upload())
