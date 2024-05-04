2be6d5528fbd1ecae4e0dd16e68bb323e7e49fa2
sookfun <sookfun.mah@gmail.com>
2024-05-04 09:40:07 +0800

    change allowedOrigins website

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 // CORS configuration
 const allowedOrigins = [
-    'https://wbfront-i7i1v4z1z-sook-funs-projects.vercel.app',
+    'https://wb-frontend-p502x5sp0-sook-funs-projects.vercel.app/',
     'http://localhost:3000' // Allow requests from your local development environment
   ];
   
